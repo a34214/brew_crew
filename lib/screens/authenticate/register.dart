64e3530ac1381216233acb5e5a135bf88ebc95b9
ali <a8907678978@gmail.com>
2024-07-03 15:50:18 +0300

    Some theme

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -26,12 +26,10 @@ class _RegisterState extends State<Register> {
     return loading
         ? const Laoding()
         : Scaffold(
-            // backgroundColor: Colors.brown[100],
             appBar: AppBar(
               backgroundColor: Theme.of(context).colorScheme.inversePrimary,
               title: const Text(
                 'Sign up to Brew Crew',
-                // style: TextStyle(color: Colors.white),
               ),
               actions: [
                 TextButton.icon(
